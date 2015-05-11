5d5c53b48ff4d385fb85f402f15f9d027925c09c
Manjunath Kaliwal <manjunathkaliwal@Manjunaths-MBP.mshome.net>
2015-05-12 00:06:09 +0530

    Initial Commit

---
+++ AutoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout
+//
+//  Created by Manjunath Kaliwal on 5/12/15.
+//  Copyright (c) 2015 Manjunath Kaliwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout
+//
+//  Created by Manjunath Kaliwal on 5/12/15.
+//  Copyright (c) 2015 Manjunath Kaliwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
